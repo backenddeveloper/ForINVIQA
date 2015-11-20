6baf853570c1d23efbef0bf52d2e62cf3396fd5d
Graham Turner <grahamturnernew@hotmail.com>
2015-11-20 19:58:15 +0000

    Calendar returns last day of month.

--- development/Calendar.php
+++ development/Calendar.php
@@ -17,5 +17,19 @@ class Calendar {
 		$this->dateObject->modify("+1 months") ;
 	
 	}
+	
+	public function getLastDayOfMonth(){
+	
+		$this->dateObject->modify("last day of this month") ;
+		
+		while(in_array($this->dateObject->format("l") , ["Saturday" , "Sunday"])){
+		
+			$this->dateObject->modify("-1 day") ;
+		
+		}
+		
+		return $this->dateObject->format("l d/m/Y") ;
+	
+	}
 
 }
--- tests/CalendarTest.php
+++ tests/CalendarTest.php
@@ -17,5 +17,38 @@ class CalendarTests extends \PHPUnit_Framework_TestCase {
 		$this->test->incrementMonth() ;
 		
 	}
+	
+	public function testReturnsLastWeekDayOfMonth(){
+	
+		$knownDates = Array(
+			"8/8/1970"   => "Monday 31/08/1970",    //Last day of August 1970 is a Monday
+			"3/3/1970"   => "Tuesday 31/03/1970",
+			"9/9/1970"   => "Wednesday 30/09/1970",
+			"12/12/1970" => "Thursday 31/12/1970",
+			"7/7/1970"   => "Friday 31/07/1970"
+		);
+		
+		foreach($knownDates as $month => $knownLastDayOfMonth){
+		
+			$test = new Calendar(new \DateTime($month)) ;
+			$this->assertEquals($test->getLastDayOfMonth() , $knownLastDayOfMonth) ;
+		}
+	
+	}
+	
+	public function testReturnsLastFridayOfWeekendEndingMonth(){
+	
+		$knownDates = Array(
+			"10/10/1970" => "Friday 30/10/1970", //Friday 30th October 1970 is the last Friday of the month.
+			"5/5/1970"   => "Friday 29/05/1970"
+		) ;
+		
+		foreach($knownDates as $month => $knownLastFridayOfMonth){
+		
+			$test = new Calendar(new \DateTime($month)) ;
+			$this->assertEquals($test->getLastDayOfMonth() , $knownLastFridayOfMonth) ;
+		}		
+	
+	}	
 
 }
