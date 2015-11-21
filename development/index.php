c5d5629b5be0bb602de8f9b9489b746292b218ad
Graham Turner <grahamturnernew@hotmail.com>
2015-11-21 23:15:57 +0000

    Improving the layout of index.php

--- development/index.php
+++ development/index.php
@@ -1,17 +1,27 @@
 <?php
 
 namespace ForINVIQA ;
+use \DateTime ;
 
 require "lib/Calendar.php" ;
 require "lib/File.php" ;
 
 try {
 
-	$calendar = new Calendar(new \DateTime) ;
-	$file = (isset($argv[1]) && is_string($argv[1])) ? new File($argv[1]) : new File() ;
+	$filename = (isset($argv[1])) ? $argv[1] : "output" ;
+	
+	$file = new File($filename) ;
 	$file->open() ;
+	
+	$calendar = new Calendar(new DateTime) ;
+	
+	//First the column headers.
 	$file->save(Array("Month" , "Bonus Day" , "Payday")) ;
+	
+	//Then this month.
 	$file->save(Array("This Month" , $calendar->getBonusDay() , $calendar->getLastDayOfMonth())) ;
+	
+	//Then the subsequent 12 months.
 	for($month = 1 ; $month < 13 ; $month++){
 		$monthName = $calendar->incrementMonth() ;
 		$file->save(Array($monthName , $calendar->getBonusDay() , $calendar->getLastDayOfMonth())) ;
@@ -23,4 +33,3 @@ try {
 	exit(1) ;
 
 }
-
